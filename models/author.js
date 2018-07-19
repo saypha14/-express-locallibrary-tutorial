ebf1055380eae161b7cf23801a93439360ab8fc2
saypha14 <yassinlyasa@gmail.com>
2018-07-20 02:02:07 +0300

    initial commit

---
+++ models/author.js
@@ -0,0 +1,36 @@
+var moment = require('moment');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
