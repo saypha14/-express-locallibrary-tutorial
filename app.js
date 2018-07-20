d6b168c63da273f03e9c493b8711f041cb8dbd7d
saypha14 <yassinlyasa@gmail.com>
2018-07-20 06:33:55 +0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+var express = require('express');
+var path = require('path');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog'); // Import routes for "catalog" area of site
 var compression = require('compression');
 var helmet = require('helmet');
 
@@ -5,68 +15,50 @@ var helmet = require('helmet');
 var app = express();
 
 app.use(helmet());
-...
-var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
-var compression = require('compression');
 
-// Create the Express application object
-var app = express();
-
-...
-
-app.use(compression()); //Compress all routes
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-
-var app = express();
-
-//Set up mongoose connection
+// Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://mTech:saypha14@ds243041.mlab.com:43041/local_library';
+var dev_db_url = 'mongodb://mTech:saypha14@ds243041.mlab.com:43041/local_library'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use('/', index);
+app.use('/users', users);
+app.use('/catalog', catalog); // Add catalog routes to middleware chain.
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
